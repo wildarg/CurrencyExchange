dd5b6bc4f0a5cf3e065c26bffe09dbda5df74811
Pavel Kopytin <Pavel.Kopytin@mail.ru>
2016-10-24 01:39:13 +0300

    test view pager with circle indicator

--- app/src/main/java/com/wild/currencyexchange/MainActivity.java
+++ app/src/main/java/com/wild/currencyexchange/MainActivity.java
@@ -1,13 +1,64 @@
 package com.wild.currencyexchange;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.src_currency_pager)
+    ViewPager srcPager;
+    @BindView(R.id.src_pager_indicator)
+    CircleIndicator srcIndicator;
+    @BindView(R.id.dst_currency_pager)
+    ViewPager dstPager;
+    @BindView(R.id.dst_pager_indicator)
+    CircleIndicator dstIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        srcPager.setAdapter(new CurrencyPageAdapter(getSupportFragmentManager()));
+        srcIndicator.setViewPager(srcPager);
+    }
+
+    private class CurrencyPageAdapter extends FragmentPagerAdapter {
+
+        public CurrencyPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new CurrencyFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+
+    public static class CurrencyFragment extends Fragment {
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.currency_layout, container, false);
+        }
     }
 }
